596ac8ffd483726b5d5a5bccbbc99c4aff1f463c
Juan Carlos <juanccervantes@gmail.com>
2026-08-10 21:58:27 -0600

    Disponibilidad de invitacion

--- src/features/user/pages/Invitation/Invitation.tsx
+++ src/features/user/pages/Invitation/Invitation.tsx
@@ -4,17 +4,27 @@ import styles from "./Invitation.module.css";
 /* DEPENDENCIES */
 import { type JSX } from "react";
 import { useData } from "../../hooks/useData";
+import { useTime } from "../../../../hooks/useTime";
 import { usePopupContext } from "../../hooks/usePopupContext";
 
 /* COMPONENTS */
 import { Error, Loading } from "../../../../components/ui";
 import { Cover, Background, Sections, ConfirmPopup, CancelPopup } from "../../components";
 
+const finalTime: number = new Date("2026-08-23T00:00:00.000-06:00").getTime();
+
 export const Invitation = (): JSX.Element => {
     const { popupState } = usePopupContext();
 
     // Obtiene la informacion de la invitacion
     const { isError, isLoading, invitation } = useData();
+
+    // Obtiene el tiempo maximo de validez de la invitacion
+    const { offset } = useTime(finalTime);
+
+    if(offset === 0) {
+        return <Error message="Lamentamos que no hayas aceptado tu invitación a tiempo, razón por la cual ha sido cancelada :(." />
+    }
 
     if(isLoading) {
         return <Loading />;
